6a3db5a769113803cfbc630f2f9bcceaad1f0e66
dutautes <dutasuksesif@gmail.com>
2025-03-06 12:56:56 +0700

    persis-dengan-tugas

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,20 +2,17 @@ const body = document.getElementById("bodyPage"); // body
 const checkbox = document.getElementById("checkbox"); // toggle 
 const tema = document.querySelector(".ket-tema"); // ket "change theme"
 const mainContainer = document.querySelector(".main-container"); // perubahan bg pada mc
-const containerLeft = document.querySelector(".container-left"); // container kiri 
 const footer = document.querySelector("footer"); // footer
 
 checkbox.addEventListener("change", () => {
     if (checkbox.checked) { 
         body.style.background = "#000016";
         mainContainer.style.background = "linear-gradient(to right, #2C3E50 50%, #BDC3C7 50%)"; 
-        containerLeft.style.color = "white";
         footer.style.color = "white";
         tema.style.color = "white";
     } else { 
         body.style.background = "#fff";
-        mainContainer.style.background = "linear-gradient(to right, #FFFACD 50%, white 50%)";
-        containerLeft.style.color = "black";
+        mainContainer.style.background = "linear-gradient(to right, rgb(77, 146, 169) 50%, white 50%)";
         footer.style.color = "black";
         tema.style.color = "black";
     }
